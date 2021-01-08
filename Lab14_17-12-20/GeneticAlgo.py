68f76c06a1ddb89b87a236b88e36284460cb2246
parthnvaswani <parthnvaswani@gmail.com>
2021-01-08 08:30:58 +0530

    Lab 14 complete

---
+++ Lab14_17-12-20/GeneticAlgo.py
@@ -0,0 +1,50 @@
+import random,datetime
+
+geneSet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
+target = "Hello World!"
+
+def generate_parent(length):
+    genes = []
+    while len(genes) < length:
+        sampleSize = min(length - len(genes), len(geneSet))
+        genes.extend(random.sample(geneSet, sampleSize))
+    return ''.join(genes)
+
+def get_fitness(guess):
+    sum = 0
+    for i in range(len(target)):
+        if target[i] == guess[i]:
+            sum = sum + 1
+    return sum
+
+def mutate(parent):
+    index = random.randrange(0, len(parent))
+    childGenes = list(parent)
+    newGene, alternate = random.sample(geneSet, 2)
+    if newGene == childGenes[index]:
+        childGenes[index] = alternate
+    else:
+        childGenes[index] = newGene
+    return ''.join(childGenes)
+
+def display(guess):
+    timeDiff = datetime.datetime.now() - startTime
+    fitness = get_fitness(guess)
+    print(f"{guess}\t{fitness}\t{timeDiff}")
+
+random.seed()
+startTime = datetime.datetime.now()
+bestParent = generate_parent(len(target))
+bestFitness = get_fitness(bestParent)
+display(bestParent)
+
+while True:
+    child = mutate(bestParent)
+    childFitness = get_fitness(child)
+    if bestFitness >= childFitness:
+        continue
+    display(child)
+    if childFitness >= len(bestParent):
+        break
+    bestFitness = childFitness
+    bestParent = child
